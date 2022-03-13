30badd95f7d962d898ec5816b866e48c9969422d
SeitoOnDaCode <101486482+SeitoOnDaCode@users.noreply.github.com>
2022-03-13 02:02:36 +0000

    Add files via upload

---
+++ LOVESOSA.py
@@ -0,0 +1,26 @@
+import webbrowser
+url = ("https://www.youtube.com/watch?v=YWyHZNBz6FE&ab_channel=DGainz")
+webbrowser.open(url)
+
+
+print ("""   Fuckers in school telling me, always in the barber shop
+"Chief Keef ain't 'bout this, Chief Keef ain't 'bout that"
+My boy a BD on fucking Lamron and them
+He, he, they say that nigga don't be putting in no work
+Shut the fuck up
+Y'all niggas ain't know shit
+All y'all motherfuckers talkin' about
+"Chief Keef ain't no hitter
+Chief Keef ain't this
+Chief Keef a fake"
+Shut the fuck up
+Y'all don't live with that nigga
+Y'all know that nigga got caught with a ratchet
+Shootin' at the police and shit
+Nigga been on probation since fuckin' I don't know when
+Motherfucker, stop fuckin' playin' him like that
+Them niggas savages out there
+If I catch another motherfucker talking sweet about Chief Keef
+I'm fucking beatin' they ass
+I'm not fucking playin' no more
+Y'all know them niggas roll with Lil Reese and them  """)
